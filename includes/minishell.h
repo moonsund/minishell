467ce5f5bfa54b359e2c680d2e458429d666b4e3
3615Prunelle <elfrasquitofresquito@gmail.com>
2025-10-25 17:10:56 +0200

    Added functions to the .h file

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,18 +1,17 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdlib.h>	// EXIT_FAILURE, EXIT_SUCCESS
-# include <limits.h>	// INT_MAX
-# include <sys/time.h>	// time
-# include <unistd.h>	// close, pipe, fork, read, write, getcwd
-# include <stdio.h>		// perror
-# include <errno.h>		// errno
-# include <sys/wait.h>	// waitpid
-# include <stdbool.h>
+#include <stdlib.h>		// EXIT_FAILURE, EXIT_SUCCESS
+#include <limits.h>		// INT_MAX
+#include <sys/time.h>	// time
+#include <unistd.h>		// close, pipe, fork, read, write, getcwd, chdir
+#include <stdio.h>		// perror
+#include <errno.h>		// errno
+#include <sys/wait.h>	// waitpid
+#include <stdbool.h>
 #include <signal.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-
 
 
 typedef struct t_shell
@@ -27,6 +26,9 @@ typedef struct t_shell
 void setup_signals(void);
 
 
-
+// exec functions
+void	execute_commands(t_shell command/*, TBD */);
+void	execute_pwd();
+void	execute_cd(char *requested_path);
 
 #endif
