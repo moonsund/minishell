ef3333582fd26886dd70941fbfa71fe7f359637c
moonsund <von.ravik@gmail.com>
2026-02-03 23:05:51 +0100

    parse_exit_code

--- sources/execution/exec_builtin_cmds_utils.c
+++ sources/execution/exec_builtin_cmds_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_builtin_cmds_utils.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: schappuy <schappuy@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lorlov <lorlov@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 20:19:08 by schappuy          #+#    #+#             */
-/*   Updated: 2026/02/03 20:48:32 by schappuy         ###   ########.fr       */
+/*   Updated: 2026/02/03 23:03:07 by lorlov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 
 char		*fetch_current_working_directory(void);
 bool		is_line_return(char **cmd, int *i);
-bool	is_numeric_string(const char *str);
 int			parse_exit_code(const char *str, int *exit_code);
+static int	parse_digits(const char *str, int i, long long *result);
+bool	is_numeric_string(const char *str);
 
 char	*fetch_current_working_directory(void)
 {
@@ -46,6 +47,46 @@ bool	is_line_return(char **cmd, int *i)
 	}
 }
 
+int	parse_exit_code(const char *str, int *exit_code)
+{
+	long long	result;
+	int			sign;
+	int			i;
+
+	if (!is_numeric_string(str))
+		return (-1);
+	result = 0;
+	sign = 1;
+	i = 0;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	i = parse_digits(str, i, &result);
+	if (i == -1)
+		return (-1);
+	result = sign * result;
+	*exit_code = (int)(result % 256);
+	if (*exit_code < 0)
+		*exit_code += 256;
+	return (0);
+}
+
+static int	parse_digits(const char *str, int i, long long *result)
+{
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (*result > (LLONG_MAX - (str[i] - '0')) / 10)
+			return (-1);
+		*result = *result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (i);
+}
+
 bool	is_numeric_string(const char *str)
 {
 	int	i;
@@ -64,38 +105,4 @@ bool	is_numeric_string(const char *str)
 		i++;
 	}
 	return (true);
-}
-
-int	parse_exit_code(const char *str, int *exit_code)
-{
-	long long	result;
-	int			sign;
-	int			i;
-
-	if (!str || !str[0])
-		return (0);
-	if (!is_numeric_string(str))
-		return (-1);
-	result = 0;
-	sign = 1;
-	i = 0;
-	if (str[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (str[i] == '+')
-		i++;
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		if (result > (LLONG_MAX - (str[i] - '0')) / 10)
-			return (-1);
-		result = result * 10 + (str[i] - '0');
-		i++;
-	}
-	result = sign * result;
-	*exit_code = (int)(result % 256);
-	if (*exit_code < 0)
-		*exit_code = 256 + *exit_code;
-	return (0);
 }
