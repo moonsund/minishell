21529d6de643b82e0cd9c289d4291ddbd59f4165
moonsund <von.ravik@gmail.com>
2026-02-03 22:46:42 +0100

    minishell folder added

---
+++ sources/minishell/minishell.c
@@ -0,0 +1,88 @@
+#include "minishell.h"
+
+void minishell_loop(t_shell *shell);
+char *handle_input(void);
+void	process_input(char *input, t_shell *shell);
+int	parse_and_prepare(t_shell *shell, char *input);
+int	execute_prepared(t_shell *shell);
+
+void minishell_loop(t_shell *shell)
+{
+    char    *input;
+
+    while (!shell->should_exit)
+    {
+        input = handle_input();
+        if (!input)
+        {
+            shell->should_exit = true;
+            break;
+        }
+
+        if (g_sigint)
+        {
+            g_sigint = 0;
+            shell->exit_status = ES_SIGINT;
+            free(input);
+            reset_iteration(shell);
+            continue;
+        }
+
+        process_input(input, shell);
+
+        free(input);
+        reset_iteration(shell);
+    }
+}
+
+char *handle_input(void)
+{
+	char *line;
+
+	line = readline("minishell> ");
+	if (!line)
+		return (NULL);
+
+	if (line && *line)
+		add_history(line);
+	return (line);
+}
+
+void process_input(char *input, t_shell *shell)
+{
+    t_exit_status status;
+
+    status = (t_exit_status)parse_and_prepare(shell, input);
+    if (status == ES_SUCCESS)
+        status = (t_exit_status)execute_prepared(shell);
+
+    shell->exit_status = status;
+}
+
+int	parse_and_prepare(t_shell *shell, char *input)
+{
+	int	status;
+
+	status = tokenize_with_qmap(input, &shell->tokens);
+	if (status != ES_SUCCESS)
+		return (status);
+
+	status = expand_tokens(&shell->tokens, &shell->env_vars, shell->exit_status);
+	if (status != ES_SUCCESS)
+		return (status);
+
+	status = build_pipeline_from_tokens(shell);
+	if (status != ES_SUCCESS)
+		return (status);
+
+	status = process_heredoc(shell->pipeline, &shell->env_vars, shell->exit_status);
+    if (status != ES_SUCCESS)
+        return (status);
+
+	return (ES_SUCCESS);
+}
+
+int	execute_prepared(t_shell *shell)
+{
+	return (execute_pipeline(shell));
+}
---
+++ sources/minishell/utils.c
@@ -0,0 +1,119 @@
+#include "minishell.h"
+
+void reset_iteration(t_shell *shell);
+void shell_destroy(t_shell *shell);
+void free_tokens(t_token_list *list);
+void free_pipeline(t_pipeline *pl);
+void free_env_var_list(t_env_var_list *vars);
+void err_print(t_exit_status type, const char *ctx);
+
+void reset_iteration(t_shell *shell)
+{
+	free_tokens(&shell->tokens);
+	free_pipeline(shell->pipeline);
+	shell->pipeline = NULL;
+}
+
+void shell_destroy(t_shell *shell)
+{
+	reset_iteration(shell);
+	free_env_var_list(&shell->env_vars);
+	rl_clear_history();
+}
+
+void free_tokens(t_token_list *list)
+{
+	t_token *cur;
+	t_token *next;
+
+	if (!list)
+		return ;
+
+	cur = list->head;
+	while (cur != NULL)
+	{
+		next = cur->next;
+		free(cur->raw_str);
+		free(cur->quotes_map);
+		free(cur);
+		cur = next;
+	}
+	list->head = NULL;
+	list->tail = NULL;
+	list->count = 0;
+}
+
+void free_pipeline(t_pipeline *pl)
+{
+    size_t i;
+
+    if (!pl)
+        return;
+
+    i = 0;
+    while (i < pl->count)
+    {
+        free_cmd(&pl->cmds[i]);
+        i++;
+    }
+    free(pl->cmds);
+    free(pl);
+}
+
+void free_env_var_list(t_env_var_list *vars)
+{
+    t_var *cur;
+	t_var *next;
+
+	if (!vars)
+		return ;
+
+	cur = vars->head;
+	while (cur != NULL)
+	{
+		next = cur->next;
+		free(cur->name);
+		free(cur->value);
+        free(cur);
+		cur = next;
+	}
+	vars->head = NULL;
+    vars->tail = NULL;
+	vars->count = 0;
+}
+
+// Edited to print on stderr rather than stdout
+// TBC : typo on the printf("minishell: %s: \n", where); ?
+void err_print(t_exit_status type, const char *where)
+{
+	if (type == ES_GENERAL)
+	{
+		// printf("minishell: %s\n", where);
+		write(2, "minishell: ", 11);
+		write(2, where, ft_strlen(where));
+		write(2, "\n", 1);
+	}
+	else if (type == ES_INVALID_USAGE)
+	{
+		// printf("minishell: syntax_error: %s\n", where);
+		write(2, "minishell: syntax_error: ", 25);
+		write(2, where, ft_strlen(where));
+		write(2, "\n", 1);
+	}
+	else
+	{
+        // printf( "minishell: %s\n", where);
+		write(2, "minishell: ", 11);
+		write(2, where, ft_strlen(where));
+		write(2, "\n", 1);
+	}
+}
+
+// Edited to print on stderr rather than stdout
+void err_malloc_print(const char *where)
+{
+	// printf("minishell: %s: cannot allocate memory\n", where);
+	write(2, "minishell: ", 11);
+	write(2, where, ft_strlen(where));
+	write(2, ": cannot allocate memory\n", 26);
+}
