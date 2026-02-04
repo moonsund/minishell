c9c80558801a8dc84932d3afa070e1493b101543
3615Prunelle <elfrasquitofresquito@gmail.com>
2026-02-04 12:41:15 +0100

    envp.c needs 3 functions shortened

--- sources/envp/envp.c
+++ sources/envp/envp.c
@@ -3,28 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   envp.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lorlov <lorlov@student.42berlin.de>        +#+  +:+       +#+        */
+/*   By: schappuy <schappuy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 10:16:04 by lorlov            #+#    #+#             */
-/*   Updated: 2026/02/04 10:16:05 by lorlov           ###   ########.fr       */
+/*   Updated: 2026/02/04 12:39:42 by schappuy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_var  *find_var(t_env_var_list *list, const char *name);
-int     set_var(t_env_var_list *list, const char *name, const char *value);		// export
-int     unset_var(t_env_var_list *list, const char *name);						// unset
-char   *get_var_value(t_env_var_list *var_list, const char *var);				// my_getenv
-char  **build_envp(t_env_var_list *list);										// before execve
+t_var	*find_var(t_env_var_list *list, const char *name);
+int		set_var(t_env_var_list *list, const char *name, const char *value);
+int		unset_var(t_env_var_list *list, const char *name);
+char	*get_var_value(t_env_var_list *var_list, const char *var);
+char	**build_envp(t_env_var_list *list);
 
-t_var   *find_var(t_env_var_list *list, const char *name)
+t_var	*find_var(t_env_var_list *list, const char *name)
 {
-	t_var *cur;
+	t_var	*cur;
 
 	if (!list || !name)
 		return (NULL);
-
 	cur = list->head;
 	while (cur != NULL)
 	{
@@ -35,59 +34,49 @@ t_var   *find_var(t_env_var_list *list, const char *name)
 	return (NULL);
 }
 
-int set_var(t_env_var_list *list, const char *name, const char *value)
+int	set_var(t_env_var_list *list, const char *name, const char *value)
 {
-	t_var *variable;
-	t_var *new_variable;
-	char *new_value;
-	char *new_name;
+	t_var	*variable;
+	t_var	*new_variable;
+	char	*new_value;
+	char	*new_name;
 
 	if (!list || !name)
 		return (0);
-
 	variable = find_var(list, name);
-
 	if (variable)
 	{
 		if (!value)
 			return (1);
-
 		new_value = ft_strdup(value);
 		if (!new_value)
 			return (0);
-
 		free(variable->value);
 		variable->value = new_value;
 		return (1);
 	}
-
 	new_variable = malloc(sizeof(*new_variable));
 	if (!new_variable)
 		return (0);
-
 	new_name = ft_strdup(name);
 	if (!new_name)
 	{
 		free(new_variable);
 		return (0);
 	}
-
 	if (!value)
 		new_value = ft_strdup("");
 	else
 		new_value = ft_strdup(value);
-
 	if (!new_value)
 	{
 		free(new_name);
 		free(new_variable);
 		return (0);
 	}
-
 	new_variable->name = new_name;
 	new_variable->value = new_value;
 	new_variable->next = NULL;
-
 	if (!list->head)
 	{
 		list->head = new_variable;
@@ -99,31 +88,27 @@ int set_var(t_env_var_list *list, const char *name, const char *value)
 		list->tail = new_variable;
 	}
 	list->count++;
-
 	return (1);
 }
 
-int unset_var(t_env_var_list *list, const char *name)
+int	unset_var(t_env_var_list *list, const char *name)
 {
-	t_var *prev;
-	t_var *cur;
+	t_var	*prev;
+	t_var	*cur;
 
 	if (!list || !name)
 		return (0);
-
 	prev = NULL;
 	cur = list->head;
-
 	while (cur != NULL)
 	{
 		if (ft_strcmp(cur->name, name) == 0)
 		{
-			break;
+			break ;
 		}
 		prev = cur;
 		cur = cur->next;
 	}
-
 	if (!cur)
 		return (1);
 	else
@@ -146,15 +131,13 @@ int unset_var(t_env_var_list *list, const char *name)
 	return (1);
 }
 
-char   *get_var_value(t_env_var_list *var_list, const char *var)
+char	*get_var_value(t_env_var_list *var_list, const char *var)
 {
-	t_var *cur;
+	t_var	*cur;
 
 	if (!var_list || !var)
 		return (NULL);
-
 	cur = var_list->head;
-
 	while (cur != NULL)
 	{
 		if (ft_strcmp(cur->name, var) == 0)
@@ -164,23 +147,21 @@ char   *get_var_value(t_env_var_list *var_list, const char *var)
 	return (NULL);
 }
 
-char  **build_envp(t_env_var_list *list)
+char	**build_envp(t_env_var_list *list)
 {
-	char **envp;
-	size_t i;
-	char *tmp;
-	char *variable;
-	t_var *cur;
+	char	**envp;
+	size_t	i;
+	char	*tmp;
+	char	*variable;
+	t_var	*cur;
 
 	if (!list)
 		return (NULL);
-
 	cur = list->head;
 	i = 0;
 	envp = malloc(sizeof(*envp) * (list->count + 1));
 	if (!envp)
 		return (NULL);
-
 	while (cur != NULL)
 	{
 		tmp = ft_strjoin(cur->name, "=");
@@ -189,7 +170,6 @@ char  **build_envp(t_env_var_list *list)
 			free_envp_partial(envp, i);
 			return (NULL);
 		}
-
 		if (cur->value == NULL)
 			variable = ft_strjoin(tmp, "");
 		else
