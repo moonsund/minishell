ac2d2e93adaab0d19eed83796d91facd5a3b47e0
moonsund <von.ravik@gmail.com>
2026-02-06 11:31:02 +0100

    ctrl-c fix

--- sources/minishell/signals.c
+++ sources/minishell/signals.c
@@ -19,6 +19,7 @@ void	ctrl_c_handler(int signum)
 	(void)signum;
 	g_sigint = 1;
 	write(STDOUT_FILENO, "\n", 1);
+	rl_done = 1;
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
@@ -30,7 +31,7 @@ void	setup_signals(void)
 
 	g_sigint = 0;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
+	sa.sa_flags = 0;
 	sa.sa_handler = ctrl_c_handler;
 	sigaction(SIGINT, &sa, NULL);
 	sa.sa_handler = SIG_IGN;
